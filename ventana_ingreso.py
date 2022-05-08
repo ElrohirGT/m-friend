2b15eb501bfe11074684aa18d61dc03c006f8c22
Hayser8 <gar22076@uvg.edu.gt>
2022-05-08 13:37:27 -0600

    Validacion basica de inputs

--- ventana_ingreso.py
+++ ventana_ingreso.py
@@ -1,3 +1,4 @@
+from calendar import c
 import PySimpleGUI as sg
 
 from models import Movimiento
@@ -10,15 +11,53 @@ class VentanaIngreso():
         topPane = sg.Pane([
             sg.Col([[sg.VPush()], [sg.Text("Formulario Ingreso", font=("Mistral", 24))], [sg.VPush()]], element_justification="c"),
         ], s=(800,50))
+        botonGuardar = "btn_guardar"
+        botonRegresar = "btn_regresar"
+        a = [sg.Text("Formulario de", font=("Mistral", 24))]
+        b = [sg.Text("ingresos", font=("Mistral", 24))]
+        c = [sg.Text("Monto: ", font=("Mistral", 24)), sg.Input(key="inputmonto")]
+        d = [sg.Text("Fecha: ", font=("Mistral", 24)), sg.Input(key="inputfecha")]
+        e = [sg.Button('Guardar', key=botonGuardar)]
+        f = [sg.Button('Regresar', key=botonRegresar)]
+
+        midPane = sg.Pane([
+            sg.Col([[sg.VPush()],[sg.Push()], a, b, c, d, e, f, [sg.Push()], [sg.VPush()]], element_justification="c"),
+        ], s=(300,400))
+          
 
         layout = [
-            [topPane],
+
+            [topPane],[sg.Push(),midPane,sg.Push()]
         ]
-        window = sg.Window("Formluario Ingreso", layout)
+        window = sg.Window("Formluario Ingresos", layout)
+        
+        nuevoMovimiento = None
+
         while True:
             event, values = window.read()
             if event in (sg.WINDOW_CLOSED, 'Cancel'): # if user closes window or clicks cancel
                 break
 
+            elmonto = values["inputmonto"]
+            lafecha = values["inputfecha"]
+            xd = elmonto.isnumeric()
+            xd2 = lafecha.isalpha()
+            
+            if xd == True and xd2 == False:
+                if event == botonGuardar:
+                    movimiento = {
+                    "Monto": elmonto,
+                    "Fecha": lafecha,
+                    "Tipo": "Ingreso"
+                    }
+                nuevoMovimiento = Movimiento(movimiento)
+                break 
+            else:
+                pass
+                
+
+            if event == botonRegresar: 
+                break
+
         window.close()
-        return None
+        return nuevoMovimiento
