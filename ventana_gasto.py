bee63704b39da5d8ee9cdd85b24856b2ba27acfb
umopnadup <105020777+umopnadup@users.noreply.github.com>
2022-05-08 11:42:50 -0600

    a

--- ventana_gasto.py
+++ ventana_gasto.py
@@ -8,13 +8,29 @@ class VentanaGasto():
     # Movimiento({datos}): en caso de que sí le de a guardar
     def ObtenerGasto() -> Movimiento | None:
         topPane = sg.Pane([
-            sg.Col([[sg.VPush()], [sg.Text("Formulario Ingreso", font=("Mistral", 24))], [sg.VPush()]], element_justification="c"),
+            sg.Col([[sg.VPush()], [sg.Text("Formulario Gasto", font=("Mistral", 24))], [sg.VPush()]], element_justification="c"),
         ], s=(800,50))
+        botonGuardar = "btn_guardar"
+        botonRegresar = "btn_regresar"
+        a = [sg.Text("Formulario de", font=("Mistral", 24))]
+        b = [sg.Text("ingreso de gastos", font=("Mistral", 24))]
+        c = [sg.Text("Monto: ", font=("Mistral", 24)), sg.Input("ingrese el monto")]
+        d = [sg.Text("Fecha: ", font=("Mistral", 24)), sg.Input("Año-Mes-Día")]
+        e = [sg.Button('Guardar', key=botonGuardar)]
+        f = [sg.Button('Regresar', key=botonRegresar)]
+
+        midPane = sg.Pane([
+            sg.Col([[sg.VPush()],[sg.Push()], a, b, c, d, e, f, [sg.Push()], [sg.VPush()]], element_justification="c"),
+        ], s=(300,400))
+            
+        
 
         layout = [
-            [topPane],
+
+            [topPane],[sg.Push(),midPane,sg.Push()]
         ]
-        window = sg.Window("Formluario Ingreso", layout)
+        window = sg.Window("Formluario Gasto", layout)
+        
         while True:
             event, values = window.read()
             if event in (sg.WINDOW_CLOSED, 'Cancel'): # if user closes window or clicks cancel
