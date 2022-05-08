5791447bb9a0f2c1eeec7622dd2d76bef209d7e9
umopnadup <105020777+umopnadup@users.noreply.github.com>
2022-05-08 13:09:09 -0600

    a

--- ventana_gasto.py
+++ ventana_gasto.py
@@ -15,8 +15,8 @@ class VentanaGasto():
         botonRegresar = "btn_regresar"
         a = [sg.Text("Formulario de", font=("Mistral", 24))]
         b = [sg.Text("ingreso de gastos", font=("Mistral", 24))]
-        c = [sg.Text("Monto: ", font=("Mistral", 24)), sg.Input()]
-        d = [sg.Text("Fecha: ", font=("Mistral", 24)), sg.Input()]
+        c = [sg.Text("Monto: ", font=("Mistral", 24)), sg.Input(key="inputmonto")]
+        d = [sg.Text("Fecha: ", font=("Mistral", 24)), sg.Input(key="inputfecha")]
         e = [sg.Button('Guardar', key=botonGuardar)]
         f = [sg.Button('Regresar', key=botonRegresar)]
 
@@ -24,7 +24,6 @@ class VentanaGasto():
             sg.Col([[sg.VPush()],[sg.Push()], a, b, c, d, e, f, [sg.Push()], [sg.VPush()]], element_justification="c"),
         ], s=(300,400))
           
-        
 
         layout = [
 
@@ -32,60 +31,26 @@ class VentanaGasto():
         ]
         window = sg.Window("Formluario Gasto", layout)
         
-        Lista_de_gastos =[]
+        nuevoMovimiento = None
 
         while True:
             event, values = window.read()
             if event in (sg.WINDOW_CLOSED, 'Cancel'): # if user closes window or clicks cancel
                 break
 
-            nuevoMovimiento = None
+            
             if event == botonGuardar:
+                #otro if
+                movimiento = {
+                "Monto": values["inputmonto"],
+                "Fecha": values["inputfecha"],
+                "Tipo": "Gasto"
+                }
+                nuevoMovimiento = Movimiento(movimiento)
+                break
 
-                with open("base_de_datod_mfriend.csv", "r") as archivo:
-                    titulo = archivo.readline().rstrip().split(",")
-        
-                    numeros = archivo.readlines()
-        
-                for i in range(len(numeros)):
-                    numeros[i] = numeros[i].rstrip().split(",")
-            
-                numerosDic = {}
-        
-            
-                for columna in range(len(titulo)):
-                    numerosDic[titulo[columna]] = numeros[i][columna]
-                
-                Lista_de_gastos.append(numerosDic)
-
-                Movimiento = ({
-                "Monto": c,
-                "Tipo": "Gasto",
-                "Fecha": d
-                })
-                
-                Lista_de_gastos.append(Movimiento)
-
-                with open("base_de_datod_mfriend.csv", "w") as archivo:
-                    for i in range(len(titulo)):
-                        archivo.write(titulo[i])
-                    if i < len(titulo) - 1:
-                        archivo.write(",")
-                    else:
-                        archivo.write("\n")
-            
-                for seriesDic in Lista_de_gastos:
-                    for i in range(len(titulo)):
-                        archivo.write(numerosDic[titulo[i]])
-                        if i < len(titulo) - 1:
-                            archivo.write(",")
-                        else:
-                            archivo.write("\n")
-
-                
-
-            if event == botonRegresar:
-                nuevoMovimiento =  VentanaGasto.close()
+            if event == botonRegresar: 
+                break
 
         window.close()
-        return None
+        return nuevoMovimiento
